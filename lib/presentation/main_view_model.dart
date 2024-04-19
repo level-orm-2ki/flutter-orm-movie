85cef9823a3d20f89f16aacf5d9c3ddb3696e90c
hee462 <a01080809887@gmail.com>
2024-04-19 21:31:47 +0900

    [MODIFY] mainViewModel getUpComingMovieInfo() 변경사항 수정

--- lib/presentation/main_view_model.dart
+++ lib/presentation/main_view_model.dart
@@ -3,15 +3,15 @@ import 'package:level_ormmovie/domain/model/movie.dart';
 import 'package:level_ormmovie/domain/repository/movie_repository.dart';
 
 class MovieViewModel with ChangeNotifier {
-  final MovieRepository _repository;
+  final MovieRepository _movieRepository;
   List<Movie> movies = [];
 
   MovieViewModel({
     required MovieRepository repository,
-  }) : _repository = repository;
+  }) : _movieRepository = repository;
 
   Future<void> getUpComingMovieInfo() async {
-    movies = await _repository.getMovieInfoData();
+    movies = await _movieRepository.getMovieInfoData();
     notifyListeners();
   }
 }
