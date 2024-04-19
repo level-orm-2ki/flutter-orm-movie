527de041f7f980a90892d54d4d939e9d26f5d89e
hee462 <a01080809887@gmail.com>
2024-04-19 17:08:43 +0900

    [ADD] movie repository 생성

--- lib/data/repository/movie_repository_impl.dart
+++ lib/data/repository/movie_repository_impl.dart
@@ -1,5 +1,21 @@
+import 'package:level_ormmovie/data/api/movie_api.dart';
+import 'package:level_ormmovie/data/mapper/movie_mapper.dart';
+import 'package:level_ormmovie/domain/model/movie.dart';
 import '../../domain/repository/movie_repository.dart';
 
-class MovieRepositoryImpl implements MovieRepository{
+class MovieRepositoryImpl implements MovieRepository {
+  final MovieApi _api;
 
+  const MovieRepositoryImpl({
+    required MovieApi api,
+  }) : _api = api;
+
+  @override
+  Future<List<Movie>> getMovieInfoData() async {
+    final dto = await _api.getMovieInfo();
+    if (dto.results == null) {
+      throw Exception('영화 정보를 가져오는데 실패했습니다.');
+    }
+    return dto.results!.map((e) => e.toMovie()).toList();
+  }
 }
