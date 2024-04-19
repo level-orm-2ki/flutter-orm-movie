0288c275c031323930f099e255b1cb4abecc233b
hee462 <a01080809887@gmail.com>
2024-04-19 17:08:34 +0900

    [ADD] movie api 생성

--- lib/data/api/movie_api.dart
+++ lib/data/api/movie_api.dart
@@ -1,10 +1,25 @@
+import 'dart:convert';
 import 'package:level_ormmovie/core/config/themoviedb_config.dart';
+import 'package:http/http.dart' as http;
+import '../dto/movie_dto.dart';
 
 class MovieApi {
   final headers = {
     'accept': 'application/json',
-    'Authorization':
-        'Bearer ${TheMovieDdConfig.accessToken}'
+    'Authorization': 'Bearer ${TheMovieDdConfig.accessToken}'
   };
 
+  Future<MovieDto> getMovieInfo() async {
+    final String apiUrl =
+        '${TheMovieDdConfig.baseUrl}/3/movie/upcoming?language=ko&page=1';
+    final http.Response response = await http.get(
+      Uri.parse(apiUrl),
+      headers: headers,
+    );
+    if (response.statusCode != 200) {
+      throw Exception('TheMoviedb api error');
+    }
+    final json = jsonDecode(response.body);
+    return MovieDto.fromJson(json);
+  }
 }
