9da226e5189829bfb09e44d591f5ad75e207d2d9
hee462 <a01080809887@gmail.com>
2024-04-23 18:25:04 +0900

    [ADD] diSetup()주입

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:level_ormmovie/data/api/movie_ganre_api.dart';
 import 'package:level_ormmovie/data/api/movie_genre_local_api.dart';
 import 'package:level_ormmovie/data/repository/movie_genre_repository_impl.dart';
 import 'package:level_ormmovie/data/repository/movie_repository_impl.dart';
+import 'package:level_ormmovie/di/di_setup.dart';
 import 'package:level_ormmovie/presentation/main_screen.dart';
 import 'package:level_ormmovie/presentation/main_view_model.dart';
 import 'package:provider/provider.dart';
@@ -13,6 +14,8 @@ import 'core/util/shared_preferences_util.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
+  diSetup();
+
   runApp(const MyApp());
 }
 
